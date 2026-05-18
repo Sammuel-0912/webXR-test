e404b303bec630c978f414f3d40dbe05d3ba1df1
Sammuel-0912 <sam60320@gmail.com>
2026-05-18 23:21:16 +0800

    feat: 重構 AR 點檢頁面，增加 UI 元素及改善使用者體驗

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import DashboardPage    from './pages/DashboardPage'
 import DeviceManagePage from './pages/DeviceManagePage'
-import ArInspectPage    from './pages/ArInspectPage'
 import './App.css'
+import ArInspectPage    from './pages/ArInspectPage' // 1. 引入新頁面
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"           element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard"  element={<DashboardPage />} />
-        <Route path="/devices"    element={<DeviceManagePage />} />
+        <Route path="/"        element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/devices"   element={<DeviceManagePage />} />
         <Route path="/ar-inspect" element={<ArInspectPage />} />
       </Routes>
     </BrowserRouter>
